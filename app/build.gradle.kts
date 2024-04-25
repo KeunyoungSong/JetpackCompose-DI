64ba500327f6e8369709c160a037dc19a99ad70c
LB-Brandon <rmsdud623@gmail.com>
2024-04-25 18:52:57 +0900

    앱 모듈 수준 Hilt 의존성 추가
    - hilt gradle plugin
    - hilt-android
    - hilt-compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("kotlin-kapt")
+    alias(libs.plugins.hilt.android.gradle.plugin)
 }
 
 android {
@@ -63,5 +64,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
 }
