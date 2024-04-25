9fed53e7c13d592696d3395c1ad6c1f3f37d4d7a
LB-Brandon <rmsdud623@gmail.com>
2024-04-26 00:01:32 +0900

    AppModule 에 Gson provider 추가

--- app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
+++ app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.example.learn_jetpackcompose_di
 
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,10 +13,13 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 class AppModule {
-
     @Singleton
     @Provides
     @Named("API_URI")
     fun provideWebAPI(): String = "https://api.github.com/"
 
+    @Singleton
+    @Provides
+    fun provideGson(): Gson =
+        GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
 }
