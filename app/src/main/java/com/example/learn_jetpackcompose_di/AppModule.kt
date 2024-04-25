bff6bc2b74d57af22f5713cd47f501c292df101c
LB-Brandon <rmsdud623@gmail.com>
2024-04-25 19:17:34 +0900

    AppModule 에 Github API URI Provider 추가
    - Hilt 는 의존성 주입 시, 함수명이 아닌 리턴 타입만 확인하기 때문에 @Name 을 적용해 String 을 반환하는 메서드 각각을 구분한다

--- app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
+++ app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
@@ -1,11 +1,19 @@
 package com.example.learn_jetpackcompose_di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class AppModule {
+
+    @Singleton
+    @Provides
+    @Named("API_URI")
+    fun provideWebAPI(): String = "https://api.github.com/"
 
 }
