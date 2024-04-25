fe97c3fddfb60d4c811388c2e89dc1bcb54fffa0
LB-Brandon <rmsdud623@gmail.com>
2024-04-25 18:49:54 +0900

    Hilt 플러그인을 적용하기 위해 kapt 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -62,4 +63,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
