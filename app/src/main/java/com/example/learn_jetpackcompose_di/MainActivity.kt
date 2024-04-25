fcb601cb416f40beda56543ac38d6dfc49f1cf46
LB-Brandon <rmsdud623@gmail.com>
2024-04-25 17:11:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/learn_jetpackcompose_di/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.learn_jetpackcompose_di
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learn_jetpackcompose_di.ui.theme.LearnJetpackComposeDITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnJetpackComposeDITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LearnJetpackComposeDITheme {
+        Greeting("Android")
+    }
+}
