43f0d9f11d5f641396aceff99f3945800f6ca1da
LB-Brandon <rmsdud623@gmail.com>
2024-04-26 00:10:43 +0900

    AppModule 에 Retrofit gson converter factory provider 추가

--- app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
+++ app/src/main/java/com/example/learn_jetpackcompose_di/AppModule.kt
@@ -7,6 +7,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Converter
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -22,4 +24,8 @@ class AppModule {
     @Provides
     fun provideGson(): Gson =
         GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(gson: Gson): Converter.Factory = GsonConverterFactory.create(gson)
 }
